caf81202ddc5dfedff8fd8b019881e7945a5f21b
oromeraa <oromeraa@uoc.ub>
2026-05-03 16:11:56 +0200

    app compo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from './app.reducers';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'blog-uoc-project-front';
+  isLoading: boolean = false;
+
+  constructor(private store: Store<AppState>) {}
+  ngOnInit() {
+    this.store.select('auth').subscribe((auth) => {
+      if (auth.loading) {
+        this.isLoading = true;
+      } else {
+        this.isLoading = false;
+      }
+    });
+
+    this.store.select('posts').subscribe((posts) => {
+      if (posts.loading) {
+        this.isLoading = true;
+      } else {
+        this.isLoading = false;
+      }
+    });
+
+    this.store.select('user').subscribe((user) => {
+      if (user.loading) {
+        this.isLoading = true;
+      } else {
+        this.isLoading = false;
+      }
+    });
+
+    this.store.select('categories').subscribe((category) => {
+      if (category.loading) {
+        this.isLoading = true;
+      } else {
+        this.isLoading = false;
+      }
+    });
+  }
 }
